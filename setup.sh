ed0cadb78513523425d9d8b927105e914081460a
Ricardo Gaete <ricardo.gaete@walmart.com>
2020-11-13 12:02:59 -0300

    Adding prometheus files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
-minikube start
+#!/bin/sh
+
+set -e
+
+minikube start -p walmart --memory 4g --cpus 2
+
+kubectl apply -f namespaces.yaml
+
 helm repo add concourse https://concourse-charts.storage.googleapis.com/
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add stable https://charts.helm.sh/stable
+
 helm repo update
-helm install concourse concourse/concourse -f concourse.yaml    
+
+helm install concourse concourse/concourse -f concourse.yaml -n concourse --version 14.2.0
+helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring --version 11.1.1
